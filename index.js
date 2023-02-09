b23921e726b5780d04c0f0cf7486b5c7bfbbbfbe
Nicolas Escalante <nlante@gmail.com>
2023-02-09 15:54:56 -0300

    support ctrl

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 var keymap = {
-  13: 'enter',
-  27: 'esc',
-  32: 'space'
+  13: "enter",
+  27: "esc",
+  32: "space",
 };
 
 module.exports = isKeyCombo;
 
-function isKeyCombo(e, combo) {
-  var keys = [];
-  var key = getKey(e.which || e.keyCode || e.charCode);
-  var comboArr = combo.split('+');
+let isMacPlatform;
+
+try {
+  isMacPlatform = /(Mac|iPhone|iPod|iPad)/i.test(global.navigator.platform);
+} catch (error) {
+  isMacPlatform = false;
+}
+
+function isKeyCombo(e, combo, isMac = isMacPlatform) {
+  const keys = [];
+  const key = getKey(e.which || e.keyCode || e.charCode);
+  const comboArr = combo.split("+");
 
   if (e.shiftKey) {
-    keys.push('shift');
+    keys.push("shift");
   }
 
   if (e.altKey) {
-    keys.push('alt');
+    keys.push("alt");
   }
 
-  if (e.ctrlKey || e.metaKey) {
-    keys.push('cmd');
+  if (e.metaKey) {
+    keys.push("cmd");
+  }
+
+  if (!isMac && e.ctrlKey) {
+    keys.push("cmd");
+  } else if (e.ctrlKey) {
+    keys.push("ctrl");
   }
 
   keys.push(key);
@@ -33,10 +47,12 @@ function getKey(code) {
 }
 
 function isEqual(arr1, arr2) {
-  return arr1.length === arr2.length &&
+  return (
+    arr1.length === arr2.length &&
     arr1.every(function (obj1) {
       return arr2.some(function (obj2) {
         return obj1.toLowerCase() === obj2.toLowerCase();
       });
-    });
+    })
+  );
 }
--- test/suite.js
+++ test/suite.js
@@ -1,69 +1,105 @@
-const assert = require('assert');
-const isKeyCombo = require('../index');
+const assert = require("assert");
+const isKeyCombo = require("../index");
 
-describe('is-combo-key', function () {
-  it('shift', function () {
-    const result = isKeyCombo({
-      shiftKey: true,
-      keyCode: 27
-    }, 'shift+esc');
+describe("is-combo-key", function () {
+  it("shift", function () {
+    const result = isKeyCombo(
+      {
+        shiftKey: true,
+        keyCode: 27,
+      },
+      "shift+esc"
+    );
     assert(result);
   });
 
-  it('ctrl', function () {
-    const result = isKeyCombo({
-      ctrlKey: true,
-      keyCode: 13
-    }, 'cmd+enter');
+  it("ctrl", function () {
+    const result = isKeyCombo(
+      {
+        ctrlKey: true,
+        keyCode: 13,
+      },
+      "cmd+enter"
+    );
     assert(result);
   });
 
-  it('meta', function () {
-    const result = isKeyCombo({
-      metaKey: true,
-      keyCode: 97
-    }, 'cmd+a');
+  it("meta", function () {
+    const result = isKeyCombo(
+      {
+        metaKey: true,
+        keyCode: 97,
+      },
+      "cmd+a"
+    );
     assert(result);
   });
 
-  it('alt', function () {
-    const result = isKeyCombo({
-      altKey: true,
-      keyCode: 32
-    }, 'alt+space');
+  it("alt", function () {
+    const result = isKeyCombo(
+      {
+        altKey: true,
+        keyCode: 32,
+      },
+      "alt+space"
+    );
     assert(result);
   });
 
-  it('combination', function () {
-    const result = isKeyCombo({
-      ctrlKey: true,
-      shiftKey: true,
-      keyCode: 98
-    }, 'cmd+shift+b');
+  it("ctrl", function () {
+    const result = isKeyCombo(
+      {
+        ctrlKey: true,
+        keyCode: 32,
+      },
+      "ctrl+space",
+      true
+    );
     assert(result);
   });
 
-  it('mayus string', function () {
-    const result = isKeyCombo({
-      metaKey: true,
-      keyCode: 98
-    }, 'CMD+B');
+  it("combination", function () {
+    const result = isKeyCombo(
+      {
+        ctrlKey: true,
+        shiftKey: true,
+        keyCode: 98,
+      },
+      "cmd+shift+b"
+    );
     assert(result);
   });
 
-  it('mayus code', function () {
-    const result = isKeyCombo({
-      metaKey: true,
-      keyCode: 90
-    }, 'cmd+z');
+  it("mayus string", function () {
+    const result = isKeyCombo(
+      {
+        metaKey: true,
+        keyCode: 98,
+      },
+      "CMD+B"
+    );
     assert(result);
   });
 
-  it('invalid', function () {
-    const result = isKeyCombo({
-      ctrlKey: true,
-      keyCode: 98
-    }, 'alt+b');
+  it("mayus code", function () {
+    const result = isKeyCombo(
+      {
+        metaKey: true,
+        keyCode: 90,
+      },
+      "cmd+z"
+    );
+    assert(result);
+  });
+
+  it("invalid", function () {
+    const result = isKeyCombo(
+      {
+        ctrlKey: true,
+        keyCode: 98,
+      },
+      "alt+b"
+    );
     assert(!result);
   });
 });
